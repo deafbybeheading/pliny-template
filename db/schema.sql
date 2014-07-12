0ec1d262daadec195aee971902b950490a7f95a8
Maciek Sakrejda <m.sakrejda@gmail.com>
2014-07-12 14:58:53 -0700

    Drop extension comments
    Commenting on extensions requires being the extension owner, which can be a problem in some environment like Heroku Postgres.

--- db/schema.sql
+++ db/schema.sql
@@ -16,13 +16,6 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
---
 -- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -32,11 +25,3 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
 --
 -- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: -
 --
-
-COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
-
-
---
--- PostgreSQL database dump complete
---
-
